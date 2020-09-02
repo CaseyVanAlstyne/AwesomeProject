da7f32b791031bcfa41ffe8b46e7d820d1342297
Casey VanAlstyne <61564549+CaseyVanAlstyne@users.noreply.github.com>
2020-09-01 21:00:03 -0400

    initial commit for console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AwesomeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Goodbye Cruel World");
+        }
+    }
+}
